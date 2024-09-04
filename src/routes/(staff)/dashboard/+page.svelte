3c6e02a1a57129cb2fb0a90c5bf3b2c4f7e760e0
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-09-04 20:48:44 +0700

    FOMS-1 | CRUD UI Company | Company

---
+++ src/routes/(staff)/dashboard/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import request from '../../../utils/request';
+
+	let sellerDatas: Array<{ name: string; id: string }> = [];
+	let loading: boolean = true;
+
+	onMount(async () => {
+		try {
+			const response = await request.get(
+				`https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json`
+			);
+			sellerDatas = response.data;
+		} catch (error) {
+			console.error('Error fetching seller data:', error);
+		} finally {
+			loading = false;
+			console.log('Loading status:', loading);
+		}
+	});
+
+	$: console.log('Data outside onMount:', sellerDatas);
+</script>
+
+{#if loading}
+	<h1>Loading</h1>
+{:else}
+	<ul>
+		{#each sellerDatas as province}
+			<li>{province.name} | {province.id}</li>
+		{/each}
+	</ul>
+{/if}
