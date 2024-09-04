e83a7dc623d894f47739e56dadbb08277d567cd3
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-09-04 21:01:42 +0700

    FOMS-2 | CRUD UI Position | Position

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,31 @@
 import autoAdapter from '@sveltejs/adapter-auto';
-import staticAdapter from '@sveltejs/adapter-static';
+// import staticAdapter from '@sveltejs/adapter-static';
 import netlifyAdapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
-import 'dotenv/config'
+import 'dotenv/config';
 
 const config = {
 	kit: {
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		},
 		// adapter: process.env.BASE_PATH !== '' && process.env.BASE_PATH !== undefined ? staticAdapter() : autoAdapter(),
-		adapter: process.env.NODE_ENV === 'development' ? netlifyAdapter({
-			edge: false,
-			split: false
-		}) : autoAdapter(),
+		adapter:
+			process.env.NODE_ENV === 'development'
+				? netlifyAdapter({
+						edge: false,
+						split: false
+					})
+				: autoAdapter(),
 		paths: {
 			base: process.env.BASE_PATH
 		}
