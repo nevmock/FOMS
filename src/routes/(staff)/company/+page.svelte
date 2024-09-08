16900e144a21463d14fcdf02998dc18c13b5dce1
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-09-08 22:25:31 +0700

    FOMS-2 | CRUD UI List Company | Company

--- src/routes/(staff)/company/+page.svelte
+++ src/routes/(staff)/company/+page.svelte
@@ -130,28 +130,28 @@
 					<ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
 						<li>
 							<a
-								href="#"
+								href="/comapany"
 								class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
 								>Previous</a
 							>
 						</li>
 						<li>
 							<a
-								href="#"
+								href="/comapany"
 								class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
 								>1</a
 							>
 						</li>
 						<li>
 							<a
-								href="#"
+								href="/comapany"
 								class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
 								>2</a
 							>
 						</li>
 						<li>
 							<a
-								href="#"
+								href="/comapany"
 								aria-current="page"
 								class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
 								>3</a
@@ -159,14 +159,14 @@
 						</li>
 						<li>
 							<a
-								href="#"
+								href="/comapany"
 								class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
 								>4</a
 							>
 						</li>
 						<li>
 							<a
-								href="#"
+								href="/comapany"
 								class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
 								>Next</a
 							>
