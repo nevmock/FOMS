939998e11b0c3e0894bc7cdef14d0a9981e8cad2
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-09-09 10:36:46 +0700

    FOMS-17 | CRUD UI List User | User

--- src/routes/(staff)/user/+page.ts
+++
@@ -1,8 +0,0 @@
-// src/routes/+page.ts
-
-import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from '../../$types';
-
-export const load: PageLoad = () => {
-	throw redirect(302, '/user/create');
-};
