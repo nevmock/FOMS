b47bdffe5f824f8e7af357854cad271fc0dfbdee
kevinirawand <kevinid07@gmail.com>
2024-12-09 23:00:45 +0700

    feature: fix relation position company

--- src/routes/api/v1/position/GET.ts
+++ src/routes/api/v1/position/GET.ts
@@ -1,16 +1,13 @@
 import { Endpoint, z } from 'sveltekit-api';
 import { ZodResponse, Query } from '$lib/server/schema/http';
 import PositionService from '$lib/server/domain/position/service';
-import type { OurPayload } from '$lib/server/types/request';
 import { snakeToCamel } from '$lib/server/utils/caseParser';
 import { composeResponse } from '$lib/server/utils/response';
-import type { Position } from '@prisma/client';
-import type { OurResponse } from '$lib/server/types/response';
-import { positionSchema } from '$lib/server/schema/position';
+import { positionResponse } from '$lib/server/schema/position';
 
 const _services = new PositionService();
 
-const Output = ZodResponse(positionSchema);
+const Output = ZodResponse(positionResponse);
 export default new Endpoint({ Query, Output }).handle(async (param, { request }) => {
 	const url = new URL(request.url);
 
